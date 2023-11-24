8e92e63862624025292c13b8aafd3c9d2c38e1ea
miriamemmanuel21 <151394660+miriamemmanuel21@users.noreply.github.com>
2023-11-24 08:27:32 +0100

    Add files via upload
    tasks

---
+++ Heart calculator.py
@@ -0,0 +1,9 @@
+age = int(input("please enter your age: ")
+max_heart_rate = 220 - age
+
+min_target_rate = 0.5*
+max_heart_rate # 50% of max heart rate
+
+max_target_rate = 0.85*
+max_heart_rate #85% of max heart rate
+print("Your target heart rate range is between", min_target_rate,"and", max_target_rate,"beats per minute")
